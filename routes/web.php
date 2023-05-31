946d1fd5e830629a5cdb113aba28f85f18bbdd78
= <=>
2023-05-31 13:44:43 +0700

    Feat : Login

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use App\Models\User;
 use Illuminate\Support\Facades\Route;
@@ -14,24 +15,29 @@ use Illuminate\Support\Facades\Route;
 | be assigned to the "web" middleware group. Make something great!
 |
 */
-
 Route::get('/', function () {
-    return view('login');
-})->name('login');
+    return view('homepage');
+})->name('home');
+
+Route::post('/login', [LoginController::class, 'authenticate']
+)->name('login');
+
 Route::get('/register', function () {
     return view('register');
 })->name('register');
+
 Route::post('/register',[UserController::class,'store']
 )->name('register-in');
-Route::get('/home', function () {
-    return view('homepage');
-})->name('home');
+
 Route::get('/about', function () {
     return view('about');
 })->name('about');
+
 Route::get('/owner', function () {
     return view('owner');
 })->name('owner');
+
 Route::get('/ownerregis', function () {
     return view('ownerregister');
 })->name('ownerregister');
+
