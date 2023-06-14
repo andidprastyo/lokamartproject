7806cb803b10380a14e101dc9bdd3d80b7ed7a42
zakyuf <mochammadzakyz@gmail.com>
2023-06-14 12:59:04 +0700

    fix script.js

--- public/js/script.js
+++ public/js/script.js
@@ -56,15 +56,6 @@ const selectedOption = event.target.value;
 dropdown.options[0].text = selectedOption;
 });
 
-const dropdown = document.querySelector('select');
-const radioInputs = document.querySelectorAll('input[type="radio"]');
-
-// Set initial placeholder text
-dropdown.addEventListener('change', (event) => {
-  const selectedOption = event.target.value;
-  dropdown.options[0].text = selectedOption;
-});
-
 // Update placeholder text when a radio input is clicked
 radioInputs.forEach((radioInput) => {
   radioInput.addEventListener('click', (event) => {
